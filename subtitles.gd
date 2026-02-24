fd1e39aaa4bc0e2e45ee69961faccb2624daf82f
nottnottloop <6772127+nottnottloop@users.noreply.github.com>
2026-02-24 20:42:42 +0000

    Stop using idx for everything and join the rest of the world

--- subtitles.gd
+++ subtitles.gd
@@ -6,12 +6,12 @@ var finishedText = ""
 func _ready() -> void:
 	pass
 
-var idx = 0
+var i = 0
 var lastLetter = Time.get_ticks_msec()
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if idx <= len(finishedText)-1 and Time.get_ticks_msec()-lastLetter> 50/Engine.time_scale and finishedText:
-		if finishedText[idx]  in ",.-":
+	if i <= len(finishedText)-1 and Time.get_ticks_msec()-lastLetter> 50/Engine.time_scale and finishedText:
+		if finishedText[i]  in ",.-":
 			lastLetter = Time.get_ticks_msec()+120/Engine.time_scale
 
 		else:
@@ -31,13 +31,13 @@ func _process(delta: float) -> void:
 			$"../../../../../../talkingMale".pitch_scale = 1.13
 			$"../../../../../../talkingMale".play()
 
-		text = "[shake  rate=4 level=8]"+finishedText.substr(0,idx+1)+"[/shake]"
-		idx += 1
+		text = "[shake  rate=4 level=8]"+finishedText.substr(0,i+1)+"[/shake]"
+		i += 1
 
 func setLine(ttext: String):
 	text = ""
 	if ":" in ttext:
-		idx = ttext.find(":")
+		i = ttext.find(":")
 	else:
-		idx = 0
+		i = 0
 	finishedText = ttext
